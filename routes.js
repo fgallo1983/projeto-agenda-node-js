ced82525ce354a24b3343e90840c15b507d4d583
Felipe <felipe_gallo@yahoo.com>
2025-06-07 17:58:12 -0300

    fix(routes): controller do login

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const homeController = require('./src/controllers/loginController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
 route.get('/', homeController.index);
