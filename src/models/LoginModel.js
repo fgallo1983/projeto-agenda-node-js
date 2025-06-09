9e38340284cd383da5133475607abaeec797a40c
Felipe <felipe_gallo@yahoo.com>
2025-06-09 07:25:22 -0300

    feat (model) criada classe do login

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -8,7 +8,9 @@ const LoginSchema = new mongoose.Schema({
 const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
-
+  constructor(body) {
+    this.body = body;
+  }
 }
 
 module.exports = Login;
