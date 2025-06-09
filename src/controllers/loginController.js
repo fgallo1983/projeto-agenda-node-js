bb045d07720311d02605c248a72aea369e545b4d
Felipe <felipe_gallo@yahoo.com>
2025-06-09 08:32:15 -0300

    feat (controller) mensagens de erro

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,8 +5,17 @@ exports.index = (req, res) => {
   return;
 };
 
-exports.register = function(req, res) {
+exports.register = async function(req, res) {
   const login = new Login(req.body);
-  login.register();
+  await login.register();
+
+  if(login.errors.length > 0) {
+    req.flash('errors', login.errors);
+    req.session.save(function(){ 
+      return res.redirect('back');
+    });
+    return;
+  }
+
   res.send(login.errors);
 }
