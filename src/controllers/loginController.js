57bcb035aa6b8af4b1f8c309ee99ca732e6cb71c
Felipe <felipe_gallo@yahoo.com>
2025-06-09 07:28:50 -0300

    feat (controller)

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,11 @@
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
   res.render('login');
   return;
 };
 
 exports.register = function(req, res) {
+  const login = new Login(req.body);
   res.send(req.body);
 }
