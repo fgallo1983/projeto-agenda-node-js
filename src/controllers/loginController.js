3bcf631049e435348c3ae6af773912c800b3f5fd
Felipe <felipe_gallo@yahoo.com>
2025-06-09 17:33:31 -0300

    feat (loginController) atualizado login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -26,5 +26,27 @@ exports.register = async function(req, res) {
     console.log(e);
     return res.render('404');
   }
+}
 
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+  if(login.errors.length > 0) {
+    req.flash('errors', login.errors);
+    req.session.save(function(){ 
+      return res.redirect('/login/index'); 
+    });
+    return;
+  }
+
+  req.flash('success', 'Seu usuário foi criado com');
+      req.session.save(function(){ 
+        return res.redirect('/login/index'); 
+      });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
 }
