607c8f327c9a1c98f0c726bb8f790c8ad6a85fa4
Felipe <felipe_gallo@yahoo.com>
2025-06-11 18:15:49 -0300

    feat (contatoController) chama o fomrulário de cadastro

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -5,12 +5,22 @@ exports.index = (req, res) => {
 };
 
 exports.register = async (req, res) => {
-    const contato = new Contato(req.body);
-    await contato.register();
-    
-    if (contato.errors.length > 0) {
-        req.flash('errors', contato.errors);
-        req.session.save(() => res.redirect('/'));
-        return;
-    }
+    try {
+
+        const contato = new Contato(req.body);
+        await contato.register();
+        
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+
+            req.flash('success', 'Contato registrado com sucesso');
+            req.session.save(() => res.redirect('back'));
+            return;
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    } 
 };
