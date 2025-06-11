84d0a3ff6e40e04ea9fc276925dacaee8b5e52c5
Felipe <felipe_gallo@yahoo.com>
2025-06-11 18:06:39 -0300

    feat (models) incluido model do contato

---
+++ src/models/ContatoModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ContatoSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  sobrenome: { type: String, required: false, default: '' },
+  email: { type: String, required: false, default: '' },
+  telefone: { type: String, required: false, default: '' },
+  criadoEm: { type: Date, default: Date.now },
+});
+
+const ContatoModel = mongoose.model('Contato', ContatoSchema);
+
+function Contato(body) {
+  this.body = body;
+  this.errors = [];
+  this.contato = null;
+}
+
+Contato.prototype.register = async function () {
+  this.valida();
+  if(this.errors.length > 0) return;
+  this.contato = await ContatoModel.create(this.body);
+};
+
+ Contato.prototype.valida = function() {
+    this.cleanUp();
+    // validação
+    // e-mail valido
+    if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('E-mail inváido');
+    if(!this.body.nome) this.errors.push('Nome é um campo obrigatório');
+    if(!this.body.nome && !this.body.telefone) this.errors.push('Pelo menos um contato precisa ser enviado');
+
+    // senha
+    if (this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push('A senha precisa ter no mínimo 3 e 50 caracteres');
+    }
+  }
+
+ Contato.prototype.cleanUp = function() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string'){
+        this.body[key] == '';
+      }
+    }
+
+    this.body = {
+    nome: this.body.nome,
+    sobrenome: this.body.sobrenome,
+    email: this.body.email,
+    telefone: this.body.telefone,
+    };
+  }
+
+module.exports = Contato;
