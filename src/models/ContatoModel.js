006d8a604f012815d52e5b6337440f0dceab4a33
Felipe <felipe_gallo@yahoo.com>
2025-06-13 19:27:31 -0300

    fix (models contato) cooreção typeof

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -18,7 +18,7 @@ function Contato(body) {
 }
 
 Contato.buscaPorId = async function(id) {
-  if (!typeof id !== 'string') return;
+  if (typeof id !== 'string') return;
   const user = await ContatoModel.findById(id);
   return user;
 }
