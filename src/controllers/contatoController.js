fe8f3cba94bd4df04ddc8a000aeb1fb8259b8ada
Felipe <felipe_gallo@yahoo.com>
2025-06-13 18:57:23 -0300

    fix (models contato) mudança de rota após cadastro do conotato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -17,7 +17,7 @@ exports.register = async (req, res) => {
         }
 
             req.flash('success', 'Contato registrado com sucesso');
-            req.session.save(() => res.redirect('/contato/register'));
+            req.session.save(() => res.redirect('/contato/index/${contato.contato._id}'));
             return;
     } catch(e) {
         console.log(e);
