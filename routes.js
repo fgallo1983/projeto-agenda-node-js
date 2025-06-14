7dac2c7dcf7d68135426bb1bc97a1f26bdb9b6d2
Felipe <felipe_gallo@yahoo.com>
2025-06-14 18:33:20 -0300

    feat (routes) adicionada rota para apagar contato

--- routes.js
+++ routes.js
@@ -15,10 +15,11 @@ route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
 
-// Rotas de login
+// Rotas de contato
 route.get('/contato/index', loginRequired, contatoController.index);
 route.post('/contato/register', loginRequired, contatoController.register);
 route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
 
 module.exports = route;
