23c1879ee57f1f38fe2ab00c18c87b08af252e64
Felipe <felipe_gallo@yahoo.com>
2025-06-14 07:48:42 -0300

    feat (controller contato) edita contato pelo id

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -51,6 +51,13 @@ Contato.prototype.register = async function () {
     email: this.body.email,
     telefone: this.body.telefone,
     };
-  }
+  };
+
+  Contato.prototype.edit = async function (id) {
+    if (typeof id !== 'string') return;
+    this.valida();
+    if (this.errors.length > 0) return;
+    this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new:true });
+  };
 
 module.exports = Contato;
