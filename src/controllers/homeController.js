7d4ea802f725df05ca6359bc6a1b9dcf93c22743
Felipe <felipe_gallo@yahoo.com>
2025-06-14 18:11:44 -0300

    feat (home controller) injetando os contatos no index

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,8 @@
-exports.index = (req, res) => {
-  res.render('index');
+const Contato = require('../models/ContatoModel');
+
+exports.index = async(req, res) => {
+  const contatos = await Contato.buscaPorContatos();
+  res.render('index', { contatos });
   return;
 };
 
